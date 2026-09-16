0df4372265d88ca476ffcd8cad50adf3666338e6
Lucas Marcelino <lucasmlg@live.com>
2026-09-16 15:28:54 -0300

    Estudos sobre Spring Data JPA

---
+++ src/main/java/com/bookstore/jpa/repositories/BookRepository.java
@@ -0,0 +1,14 @@
+package com.bookstore.jpa.repositories;
+
+import com.bookstore.jpa.models.BookModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+
+public interface BookRepository extends JpaRepository<BookModel, UUID> {
+
+    BookModel findBookModelByTitle(String title);
+    BookModel findBookModelById(UUID id);
+
+}
